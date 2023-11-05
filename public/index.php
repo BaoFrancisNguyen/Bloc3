22c9728bea0feb2dafa9fea790953f61534d452b
api-scraping <bao.francis.nguyen@gmail.com>
2023-11-05 20:49:37 +0100

    add route

--- public/index.php
+++ public/index.php
@@ -10,25 +10,45 @@ session_start();
 
 require_once "../dump_anything.php";
 
-// Vérification si l'utilisateur est déjà connecté
-if (isset($_SESSION["is_logged_in"]) && $_SESSION["is_logged_in"] == 1) {
-    // Redirection vers la page d'administration si l'utilisateur est déjà connecté
-    require "../admin/admin.html";
-    exit;
-}
-
 $request_route = $_SERVER["REQUEST_METHOD"] . "_" . $_SERVER["REQUEST_URI"];
 
 switch ($request_route) {
+
     case 'GET_/':
         if(!isset($_SESSION["is_logged_in"]) || $_SESSION["is_logged_in"] != 1) {
             require "../views/login.php";
         }
-        
+        else {
+            require '../frantz_project.html';
+        }
         break;
+
+    case 'GET_/admin/admin.html':
+        //TODO check the role of the user
+        if(isset($_SESSION["is_logged_in"]) && $_SESSION["is_logged_in"] == 1 && $_SESSION["role"] == "admin") {
+            require "../admin/admin.html";
+        }
+        break;
+
+    case 'GET_/admin/admin.php':
+        if(isset($_SESSION["is_logged_in"]) || $_SESSION["is_logged_in"] == 1) {
+            require "../admin/admin.php";
+        }
+        break;
+
+    case "GET_/get_categories.php":
+        require "../get_categories.php";
+        break;
+
+    case "GET_/deconnexion.php":
+        require "../admin/deconnexion.php";
+        break;
+
     case "POST_/login":
         require "process_login.php";
         break;
+
+
     default:
         // TODO make a 404 page
         dump_anything("404 blabla");
