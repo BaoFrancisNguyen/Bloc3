f4b097b44d76ad0373a8a272125ec0987f8284b2
api-scraping <bao.francis.nguyen@gmail.com>
2023-11-05 21:23:44 +0100

    header changes

--- admin/deconnexion.php
+++ admin/deconnexion.php
@@ -1,10 +1,10 @@
 <?php
 // Supprimez la session existante
-session_start();
+
 session_unset();
 session_destroy();
 
 // Redirigez vers la page de connexion
-require "../public/index.php";
+header("Location: /");
 exit();
 ?>
