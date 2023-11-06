cef5813d00d62f499260894df8882c06d7d38dc8
api-scraping <bao.francis.nguyen@gmail.com>
2023-11-06 16:57:54 +0100

    ajout catalogue d articles dans la table article

--- data/schema.sql
+++ data/schema.sql
@@ -37,6 +37,44 @@ CREATE TABLE IF NOT EXISTS admin(
     role ENUM('admin', 'user') NOT NULL
 );
 
+INSERT INTO articles (categorie, nom, prix)
+VALUES
+    ('alimentaire', 'fruits', 10),
+    ('alimentaire', 'légumes', 5),
+    ('alimentaire', 'viandes', 15),
+    ('alimentaire', 'poissons', 10),
+    ('alimentaire', 'boissons', 3),
+    ('alimentaire', 'produits laitiers', 5),
+    ('multimédia', 'smartphones', 200),
+    ('multimédia', 'ordinateurs', 500),
+    ('multimédia', 'casques', 50),
+    ('multimédia', 'enceintes', 100),
+    ('multimédia', 'télévisions', 500),
+    ('multimédia', 'consoles', 300),
+    ('vêtements', 't-shirts', 20),
+    ('vêtements', 'jeans', 50),
+    ('vêtements', 'robes', 100),
+    ('vêtements', 'chaussures', 50),
+    ('vêtements', 'sous-vêtements', 10),
+    ('vêtements', 'manteaux', 100),
+    ('électroménager', 'réfrigérateurs', 500),
+    ('électroménager', 'lave-linges', 300),
+    ('électroménager', 'lave-vaisselle', 200),
+    ('électroménager', 'micro-ondes', 100),
+    ('électroménager', 'robots', 500),
+    ('électroménager', 'cafetières', 100),
+    ('jouets', 'poupées', 50),
+    ('jouets', 'voitures', 30),
+    ('jouets', 'peluches', 20),
+    ('jouets', 'jeux de société', 50),
+    ('jouets', 'jeux de construction', 100),
+    ('jouets', 'jeux de plein air', 50),
+    ('sport', 'vélos', 200),
+    ('sport', 'raquettes', 50),
+    ('sport', 'ballons', 20),
+    ('sport', 'haltères', 100),
+    ('sport', 'tapis de course', 500),
+    ('sport', 'skateboards', 100);
 
 --DELIMITER //
 --CREATE PROCEDURE InsertAdmin()
