2a70d30edbf0f71d657fe0c563a0c993911053cb
20100dbg <vincent.marie80@gmail.com>
2023-11-09 20:53:58 +0100

    nettoyage des fichiers

--- sql.php
+++ sql.php
@@ -5,16 +5,6 @@ $dbuser = "bloc3";
 $password = "bloc3";
 $db = "bloc3";
 
-/*
-try {
-    $dbh = new PDO("mysql:host=$dbhost;dbname=$db", $dbuser, $password);
-    echo "db connection works !";
-    $dbh = null;
-} catch (PDOException $e) {
-    print "Error!: " . $e->getMessage() . "<br/>";
-    die();
-}
-*/
 
 $conn = new mysqli($dbhost, $dbuser, $password, $db);
 
@@ -23,4 +13,3 @@ if ($conn->connect_error) {
     die("Erreur de connexion : " . $conn->connect_error);
 }
 
-?>
