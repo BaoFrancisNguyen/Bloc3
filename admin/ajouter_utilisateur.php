7cfd56a6832e0fd746aefbe0005a640839922078
api-scraping <bao.francis.nguyen@gmail.com>
2023-11-10 00:07:03 +0100

    redirection fonctionnel

--- admin/ajouter_utilisateur.php
+++ admin/ajouter_utilisateur.php
@@ -1,4 +1,5 @@
 <?php
+ob_start(); //activer la mise en tampon de sortie
 
 $username = $_POST["username"];
 $password = $_POST["password"];
@@ -11,7 +12,7 @@ $check_result = $conn->query($check_user_sql);
 
 if ($check_result->num_rows > 0) {
     echo "Cet utilisateur existe déjà dans la base de données.";
-    header("Refresh: 5; URL=/admin/index.php"); // Redirige après 5 secondes
+    header("Refresh: 5; URL=/admin/admin.php"); // Redirige après 5 secondes
     $conn->close();
     exit();
 }
@@ -21,7 +22,7 @@ $sql = "INSERT INTO admin (login, password, role) VALUES ('$username', '$hashed_
 
 if ($conn->query($sql) === TRUE) {
     echo "Utilisateur ajouté avec succès. Vous serez redirigé dans quelques secondes...";
-    header("Refresh: 5; URL=/admin/index.php"); // Redirige après 5 secondes
+    header("Refresh: 5; URL=/admin/admin.php"); // Redirige après 5 secondes
     exit();
 } else {
     echo "Erreur lors de l'ajout de l'utilisateur: " . $conn->error;
