fc7a6e4e3d53e736bce274f2f228826d74d27ec0
api-scraping <bao.francis.nguyen@gmail.com>
2023-11-14 00:07:57 +0100

    test

--- tests/test_add_random.py
+++ tests/test_add_random.py
@@ -37,4 +37,74 @@ def test_select_and_calculate_prix_panier():
     for article_id, _ in achats:
         assert article_id in [a[0] for a in articles_test], "ID d'article non valide trouvé dans les achats"
 
-# Vous pouvez ajouter d'autres tests ici
+def insert_collecte(cursor, prix_panier_initial, date_achat):
+    # Insert a new collecte and return its ID
+    cursor.execute("INSERT INTO collectes (date_achat, prix_panier) VALUES (%s, %s)", (date_achat, prix_panier_initial))
+    return cursor.lastrowid
+
+def test_insert_collecte():
+    cursor_mock = MagicMock()
+    date_achat = "2023-01-01"
+    prix_panier_initial = 100
+
+    # Supposons que lastrowid retourne 1 pour cet appel
+    cursor_mock.lastrowid = 1
+
+    collecte_id = insert_collecte(cursor_mock, prix_panier_initial, date_achat)
+
+    # Vérifiez que le cursor a été appelé correctement
+    cursor_mock.execute.assert_called_with(
+        "INSERT INTO collectes (date_achat, prix_panier) VALUES (%s, %s)", 
+        (date_achat, prix_panier_initial)
+    )
+    assert collecte_id == 1
+
+def insert_achats(cursor, achats, collecte_id):
+    # Insert details of purchased articles into 'session_articles'
+    for article_id, quantite in achats:
+        cursor.execute("INSERT INTO session_articles (article_id, quantite, collecte_id) VALUES (%s, %s, %s)",
+                       (article_id, quantite, collecte_id))
+
+def test_insert_achats():
+    cursor_mock = MagicMock()
+    achats = [(1, 2), (2, 3)]  # exemple d'achats
+    collecte_id = 1
+
+    insert_achats(cursor_mock, achats, collecte_id)
+
+    # Vérifiez que les appels à la base de données sont corrects
+    calls = [
+        ((1, 2, collecte_id),),
+        ((2, 3, collecte_id),)
+    ]
+    cursor_mock.execute.assert_has_calls([
+        (("INSERT INTO session_articles (article_id, quantite, collecte_id) VALUES (%s, %s, %s)", call),)
+        for call in calls
+    ], any_order=True)
+
+def insert_client(cursor, nb_enfants, cat_socio_pro, collecte_id):
+    # Insert client information associated with this collecte
+    cursor.execute("INSERT INTO clients (nb_enfants, cat_socio_pro, collecte_id) VALUES (%s, %s, %s)",
+                   (nb_enfants, cat_socio_pro, collecte_id))
+    return cursor.lastrowid
+
+def test_insert_client():
+    cursor_mock = MagicMock()
+    nb_enfants = 2
+    cat_socio_pro = "employé"
+    collecte_id = 1
+
+    # Supposons que lastrowid retourne 1 pour cet appel
+    cursor_mock.lastrowid = 1
+
+    client_id = insert_client(cursor_mock, nb_enfants, cat_socio_pro, collecte_id)
+
+    # Vérifiez que le cursor a été appelé correctement
+    cursor_mock.execute.assert_called_with(
+        "INSERT INTO clients (nb_enfants, cat_socio_pro, collecte_id) VALUES (%s, %s, %s)", 
+        (nb_enfants, cat_socio_pro, collecte_id)
+    )
+    assert client_id == 1
+
+
+
