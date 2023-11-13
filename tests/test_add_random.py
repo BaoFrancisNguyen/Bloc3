5aef048cd392f92994d3dae7a35656b75dbe8be9
api-scraping <bao.francis.nguyen@gmail.com>
2023-11-13 23:36:06 +0100

    test

--- tests/test_add_random.py
+++ tests/test_add_random.py
@@ -1,86 +1,24 @@
-import mysql.connector
-import random
-from datetime import datetime
 
-# Configuration for MySQL connection
-config = {
-    'user': 'root',
-    'password': 'password',
-    'host': '127.0.0.1',
-    'database': 'testdb',
-    'raise_on_warnings': True
-}
+import pytest
+from unittest.mock import MagicMock
+from add_random import select_and_calculate_prix_panier
 
-# Connect to the database
-connection = mysql.connector.connect(**config)
-cursor = connection.cursor()
+def test_select_and_calculate_prix_panier():
+    # Création d'un mock pour le cursor
+    cursor_mock = MagicMock()
+    # Définir un ensemble d'articles de test (article_id, prix)
+    articles_test = [(1, 10.0), (2, 20.0), (3, 15.0), (4, 5.0), (5, 25.0)]
+    cursor_mock.fetchall.return_value = articles_test
 
-def insert_collecte(cursor, prix_panier_initial, date_achat):
-    # Insert a new collecte and return its ID
-    cursor.execute("INSERT INTO collectes (date_achat, prix_panier) VALUES (%s, %s)", (date_achat, prix_panier_initial))
-    return cursor.lastrowid
+    # Appeler la fonction avec le mock
+    prix_panier, achats = select_and_calculate_prix_panier(cursor_mock)
 
+    # Vérifications
+    assert prix_panier > 0, "Le prix du panier devrait être supérieur à 0"
+    assert len(achats) == 5, "Le nombre d'achats devrait être égal à 5"
 
-def select_and_calculate_prix_panier(cursor, nombre_articles=5):
-    # Select random articles and calculate the prix panier
-    cursor.execute("SELECT article_id, prix FROM articles")
-    articles_disponibles = cursor.fetchall()
-    articles_achetes = random.sample(articles_disponibles, nombre_articles)
+    # Vérifier que tous les IDs d'articles dans les achats sont valides
+    for article_id, _ in achats:
+        assert article_id in [a[0] for a in articles_test], "ID d'article non valide trouvé dans les achats"
 
-    prix_panier = 0
-    achats = []
-    for article_id, prix in articles_achetes:
-        quantite = random.randint(1, 10)  # Random quantity for each article
-        prix_total_article = prix * quantite
-        prix_panier += prix_total_article
-        achats.append((article_id, quantite))  # Append a tuple with article_id and the chosen quantity
-    return prix_panier, achats
-
-def insert_achats(cursor, achats, collecte_id):
-    # Insert details of purchased articles into 'session_articles'
-    for article_id, quantite in achats:
-        cursor.execute("INSERT INTO session_articles (article_id, quantite, collecte_id) VALUES (%s, %s, %s)",
-                       (article_id, quantite, collecte_id))
-def insert_client(cursor, nb_enfants, cat_socio_pro, collecte_id):
-    # Insert client information associated with this collecte
-    cursor.execute("INSERT INTO clients (nb_enfants, cat_socio_pro, collecte_id) VALUES (%s, %s, %s)",
-                   (nb_enfants, cat_socio_pro, collecte_id))
-    return cursor.lastrowid
-
-def insertion_donnes(nb):
-
-    try:
-        for i in range(nb):
-            # Step 1: Insert a collecte with an initial prix panier of 0
-            date_achat = datetime.now().strftime("%Y-%m-%d")
-            collecte_id = insert_collecte(cursor, 0, date_achat)
-            
-            # Steps 2 and 3: Select articles and calculate the prix panier
-            prix_panier, achats = select_and_calculate_prix_panier(cursor)
-            
-            # Update the prix_panier in the collectes table
-            cursor.execute("UPDATE collectes SET prix_panier = %s WHERE collecte_id = %s", (prix_panier, collecte_id))
-            
-            # Step 4: Insert a client
-            nb_enfants = random.randint(0, 5)
-            cat_socio_pro = random.choice(['employé', 'ouvrier', 'cadre', 'artiste', 'étudiant', 'retraité'])
-            client_id = insert_client(cursor, nb_enfants, cat_socio_pro, collecte_id)
-            
-            # Step 5: Insert details of purchased articles
-            insert_achats(cursor, achats, collecte_id)
-            
-            # Commit the changes if all operations are successful
-            connection.commit()
-        
-    except mysql.connector.Error as err:
-        print(f"Erreur lors de la connexion à la base de données: {err}")
-        # Rollback changes in case of an error
-        connection.rollback()
-    finally:
-        if connection.is_connected():
-            cursor.close()
-            connection.close()
-            print("Données aléatoires insérées avec succès.")
-
-
-insertion_donnes(10)
+# Vous pouvez ajouter d'autres tests ici
