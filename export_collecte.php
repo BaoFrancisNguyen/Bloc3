69e5b06246dae2a927c670e98879a6b4ea09842b
api-scraping <bao.francis.nguyen@gmail.com>
2023-11-14 02:24:09 +0100

    ajout possibilite extract collecte

---
+++ export_collecte.php
@@ -0,0 +1,54 @@
+<?php
+// export_collecte.php
+
+// Inclure la configuration de la base de données
+include 'sql.php';
+
+// Vérifiez si l'action d'exportation a été demandée
+if (isset($_POST['export_data'])) {
+    $rowCount = isset($_POST['row_count']) ? intval($_POST['row_count']) : 0;
+    exportCollecteDataToCSV($rowCount);
+}
+
+function exportCollecteDataToCSV($rowCount) {
+    // Connexion à la base de données
+    $conn = new mysqli($dbhost, $dbuser, $password, $db);
+
+    // Vérifier la connexion
+    if ($conn->connect_error) {
+        die("Échec de la connexion : " . $conn->connect_error);
+    }
+
+    // Requête pour obtenir les données
+    $sql = "SELECT * FROM collectes LIMIT ?";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("i", $rowCount);
+    $stmt->execute();
+    $result = $stmt->get_result();
+
+    if ($result->num_rows > 0) {
+        // Ouvrir le fichier en écriture
+        $fp = fopen('php://output', 'w');
+
+        // En-tête pour forcer le téléchargement du fichier
+        header('Content-Type: text/csv');
+        header('Content-Disposition: attachment; filename="export_collecte.csv"');
+
+        // En-tête du CSV
+        fputcsv($fp, array('Collecte_id', 'Date_achat', 'Prix_panier'));
+
+        // Ajouter les lignes de données
+        while ($row = $result->fetch_assoc()) {
+            fputcsv($fp, $row);
+        }
+
+        fclose($fp);
+    } else {
+        echo "0 résultats";
+    }
+
+    $stmt->close();
+    $conn->close();
+    exit;
+}
+?>
